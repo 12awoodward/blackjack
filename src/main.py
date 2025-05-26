c8f1fd790c5fd00dfd54f2cd276d780bfb36d66e
12awoodward <203260994+12awoodward@users.noreply.github.com>
2025-05-26 16:44:38 +0100

    game settings layout

---
+++ src/main.py
@@ -0,0 +1,20 @@
+
+default = {
+    "all" : {
+        "A" : ["suit"],
+        "2" : ["pickup_add_2"],
+        "8" : ["skip"],
+        "Q" : ["direction"],
+    },
+    "black" : {
+        "J" : ["pickup_add_5"],
+    },
+    "red" : {
+        "J" : ["pickup_set_0"],
+    },
+}
+
+def main():
+    print(default)
+
+main()
