c0940fb58ea26ea00c2a105dfc47d4c6b4ba7053
12awoodward <203260994+12awoodward@users.noreply.github.com>
2025-06-02 10:57:37 +0100

    added sort hand & play card functions

--- src/player.py
+++ src/player.py
@@ -1,3 +1,18 @@
+def card_sort(card):
+    val = card.num.value.strip()
+    match val:
+        case "A":
+            val = 1
+        case "J":
+            val = 11
+        case "Q":
+            val = 12
+        case "K":
+            val = 13
+        case _:
+            val = int(val)
+    return (card.suit.value, val)
+
 class Player:
     def __init__(self, name, is_computer):
         self.name = name
@@ -7,4 +22,10 @@ class Player:
 
     def take_cards(self, cards):
         self.hand += cards
+        self.sort_hand()
 
+    def sort_hand(self):
+        self.hand.sort(key = card_sort)
+
+    def play_card(self, card_index):
+        return self.hand.pop(card_index)
