86c9a32c4b2dfcaa374c0e1aa0c86e4bd78141bc
12awoodward <203260994+12awoodward@users.noreply.github.com>
2025-06-04 11:14:40 +0100

    updated ui functions

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from player import *
 
 default = {
     "all" : {
-        # "A" : ["suit"],
+        "A" : ["suit"],
         "2" : ["pickup_add:2"],
         "8" : ["skip"],
         "Q" : ["direction"],
@@ -20,43 +20,55 @@ default = {
 
 def set_name_spacing(player, size):
     # add spacing to match longest name
-    player.display_name = player.name + " " * (size - len(player.name))
+    player.display_name = player.name + " " * (size - len(player.name))    
 
-def hand_to_str(hand, hide = False):
-    txt = ""
-    sep = " | "
-    hidden = "[?]"
-    for card in hand:
-        if hide:
-            txt += hidden + sep
-        else:
-            txt += str(card) + sep
-    return txt[:-len(sep)]
-
-def get_str_player(player, hide = False, hand = None):
+def get_str_player(player, hide = False, hand = None, selectable = False):
     if hand is None:
         hand = player.hand
-    return player.display_name + ": " + hand_to_str(hand, hide)
+
+    txt = player.display_name + ": "
+    sep = " | "
+    hidden = "[?]"
+    count = 1
+
+    for card in hand:
+        card_str = str(card)
+        if hide:
+            card_str = hidden
+        if selectable:
+            txt += f"{count}) "
+            count += 1
+        txt += card_str + sep
+
+    return txt[:-len(sep)]
 
 def print_all_players(players, current_turn = -1, length = None):
     player_str = []
-    if length is None:
-        # show players full hand
-        player_str = [get_str_player(player, player.is_computer) for player in players]
-    else:
-        # only show hands up to length
-        player_str = [get_str_player(player, player.is_computer, player.hand[:length]) for player in players]
-        
-    size = len(max(player_str, key = lambda x : len(x)))
+    size = 0
 
-    txt = ""
-    for i in range(len(player_str)):
-        spacing = "\n"
-        if i == current_turn:
-            spacing += "-"*size + "\n"
-        txt += spacing + player_str[i] + spacing
-    txt += "\n"
-    print(txt)
+    for i in range(len(players)):
+        txt = ""
+        # show card select options if not AI and current
+        selectable = i == current_turn and not players[i].is_computer
+        if length is None:
+            # show players full hand
+            txt = get_str_player(players[i], players[i].is_computer, selectable=selectable)
+        else:
+            # only show hands up to length
+            txt = get_str_player(players[i], players[i].is_computer, players[i].hand[:length], selectable)
+        
+        #keep track of longest line
+        if len(txt) > size:
+            size = len(txt)
+        
+        player_str.append(txt)
+    
+    # apply current player marking
+    if current_turn != -1:
+        marking = "-" * size
+        player_str[current_turn] = marking + "\n" + player_str[current_turn] + "\n" + marking
+
+    print("\n" + "\n\n".join(player_str) + "\n\n")
 
 def print_card_dealing(players):
     for i in range(len(players[0].hand)):
@@ -64,8 +76,21 @@ def print_card_dealing(players):
         sleep(0.3)
 
 def print_turn(game):
-    spacing = "="*15
-    print(f"{spacing}\nTop Card: {str(game.top_card)}\n{spacing}")
+    status = f" Top Card: {str(game.top_card)}"
+
+    if game.status["pickup"] > 0:
+        status += "  - +" + str(game.status["pickup"])
+
+    if game.status["skip"]:
+        status += "  - Skip"
+
+    direction = "\u21E9"
+    if game.status["direction"] < 0:
+        direction = "\u21E7"
+    status += "  - " + direction
+
+    spacing = "=" * len(status)
+    print(f"{spacing}\n{status}\n{spacing}")
     print_all_players(game.players, current_turn=game.current_turn)
 
 def main():
